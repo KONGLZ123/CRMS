396f2b17601b20ff84790799efc16937aa60cc76
KONGLZ123 <konglingzeng@qq.com>
2018-06-05 23:37:14 +0800

    word count and sort

--- WordCountAndSort/WordCountAndSort/main.cpp
+++ WordCountAndSort/WordCountAndSort/main.cpp
@@ -3,13 +3,14 @@
 #include <map>
 #include <unordered_map>
 
+// 0. 预处理：读取文件，讲英文文章单词读出，存放在不同文件
 void PreData()
 {
-    // 0. 预处理：读取文件，讲英文文章单词读出，存放在不同文件
     std::fstream inFile;
     std::fstream outFile;
     inFile.open("input.txt", std::ios::in); // 输入模式
-    outFile.open("output.txt", std::ios::ate | std::ios::app);  // 定位到文件末尾 | 追加模式
+    //outFile.open("output.txt", std::ios::ate | std::ios::app);  // 定位到文件末尾 | 追加模式
+    outFile.open("output.txt", std::ios::trunc | std::ios::out);  // 定位到文件末尾 | 追加模式
 
     char ch;
     bool flag = false;
@@ -31,6 +32,7 @@ void PreData()
     outFile.close();
 }
 
+// 1. 计算单词个数，并保存到文件
 void WordCount(std::map<std::string, int> & wordCount)
 {
     std::fstream inFile;
@@ -38,7 +40,6 @@ void WordCount(std::map<std::string, int> & wordCount)
     inFile.open("output.txt", std::ios::in);
     outFile.open("result.txt", std::ios::out | std::ios::trunc);  // 文件如果存在则覆盖
 
-    // 1. 计算单词个数，并保存到文件
     char tmp[256];
     while (!inFile.eof()) {
         inFile.getline(tmp, 256);       // getline 默认去除\n换行
@@ -59,112 +60,64 @@ struct WordCount
     int count;
 };
 
-void QuickSort(int *a, int start, int end)
+void QuickSort(std::vector<struct WordCount> &vecWordCount, 
+    std::vector<struct WordCount>::iterator start, 
+    std::vector<struct WordCount>::iterator end)
 {
-    if (start == end)
+    if (start >= end)    // i - 1， i可能为0 | i + 1, i可能为end
         return;
 
-    int tmp = a[start];
-    int i = start;
-    int j = end - 1;
-    while (i != j) {
-
+    int tmp = start->count;
+    std::string tmpStr = start->word;
+    auto i = start;
+    auto j = end - 1;   // vector end是末尾的后一个
+    while (i < j) {
+        while (i < j && j->count <= tmp) {
+            j--;
+        }
+        i->count = j->count;
+        i->word = j->word;
+        while (i < j && i->count >= tmp) {
+            i++;
+        }
+        j->count = i->count;
+        j->word = i->word;
     }
-    a[i] = tmp;
-    QuickSort(a, start, i - 1);
-    QuickSort(a, i + 1, end);
+    i->count = tmp;
+    i->word = tmpStr;
+    QuickSort(vecWordCount, start, i);     // i - 1， i可能为0 | i + 1, i可能为end
+    QuickSort(vecWordCount, i + 1, end);
 }
 
-int main(int argc, char *argv[])
+// 2. 按单词出现次数，从大到小排序
+void WordCountSort(std::map<std::string, int> & wordCount)
 {
-    //PreData();
-
-    //std::map<std::string, int> wordCount;   // 单词，个数
-    //WordCount(wordCount);
-    //
     std::vector<struct WordCount> vecWordCount;
-    //struct WordCount tmp;
-    //for (auto it : wordCount) {
-    //    tmp.word = it.first;
-    //    tmp.count = it.second;
-    //    vecWordCount.push_back(tmp);
-    //}
+    struct WordCount tmp;
+    for (auto it : wordCount) {
+        tmp.word = it.first;
+        tmp.count = it.second;
+        vecWordCount.push_back(tmp);
+    }
 
     std::fstream outFile;
     outFile.open("result2.txt", std::ios::out | std::ios::trunc);
 
-    struct WordCount tmp;
-    tmp.count = 3;
-    tmp.word = "C";
-    vecWordCount.push_back(tmp);
-    tmp.count = 1;
-    tmp.word = "A";
-    vecWordCount.push_back(tmp);
-    tmp.count = 4;
-    tmp.word = "D";
-    vecWordCount.push_back(tmp);
-    tmp.count = 2;
-    tmp.word = "B";
-    vecWordCount.push_back(tmp);
-
-    int count = vecWordCount.size();
-    auto i = vecWordCount.begin(), j = vecWordCount.end();
-    int n = 0;
-    std::string tmpWord = vecWordCount[0].word;
-    int tmpCnt = vecWordCount[0].count;
-    int t = 0;
-    std::string tmpStr;
-    
-    //while (n < count) {
-    //    i = vecWordCount.begin();
-    //    j = vecWordCount.end() - 1;
-    //    while (i != j) {
-    //        if (j->count <= tmpCnt) {
-    //            j--;
-    //        }
-    //        else {
-    //            i++;
-    //        }
-
-    //        if (i == j) {
-    //            break;
-    //        }
-
-    //        if (i->count >= tmpCnt) {
-    //            i++;
-    //        }
-    //        else {
-    //            j--;
-    //        }
-
-    //        if (i == j) {
-    //            break;
-    //        }
-    //    }
-
-    //    t = vecWordCount[i - vecWordCount.begin()].count;
-    //    vecWordCount[i - vecWordCount.begin()].count = tmpCnt;
-    //    tmpCnt = t;
-
-    //    tmpStr = vecWordCount[i - vecWordCount.begin()].word;
-    //    vecWordCount[i - vecWordCount.begin()].word = tmpWord;
-    //    tmpWord = tmpStr;
-    //    n++;
-    //}
+    QuickSort(vecWordCount, vecWordCount.begin(), vecWordCount.end());
 
     for (auto it : vecWordCount) {
         outFile << it.count << "  " << it.word.c_str() << "\n";
     }
 
     outFile.close();
-    //3. 排序
-    // 3, 1, 5, 2, 4, 6
-    // i              j
-    // 快速排序，从大到小
-    int a[] = { 3, 1, 4, 2, 2, 6, 8, 11, 3};
-    int cnt = sizeof(a) / sizeof(int);
-    QuickSort(a, 0, cnt);
-    for (int i = 0; i < cnt; ++i) {
-        std::cout << a[i] << std::endl;
-    }
+}
+
+int main(int argc, char *argv[])
+{
+    PreData();
+
+    std::map<std::string, int> wordCount;   // 单词，个数
+    WordCount(wordCount);
+
+    WordCountSort(wordCount);
 }
