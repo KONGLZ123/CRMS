32e2aaa8f0fe5417bc5c7bc0083767c78e3bbd2f
KONGLZ123 <konglingzeng@qq.com>
2018-06-05 14:48:19 +0800

    update

--- WordCountAndSort/WordCountAndSort/main.cpp
+++ WordCountAndSort/WordCountAndSort/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <unordered_map>
 
 void PreData()
 {
@@ -52,51 +53,118 @@ void WordCount(std::map<std::string, int> & wordCount)
     outFile.close();
 }
 
+struct WordCount
+{
+    std::string word;
+    int count;
+};
+
+void QuickSort(int *a, int start, int end)
+{
+    if (start == end)
+        return;
+
+    int tmp = a[start];
+    int i = start;
+    int j = end - 1;
+    while (i != j) {
+
+    }
+    a[i] = tmp;
+    QuickSort(a, start, i - 1);
+    QuickSort(a, i + 1, end);
+}
+
 int main(int argc, char *argv[])
 {
     //PreData();
 
     //std::map<std::string, int> wordCount;   // 单词，个数
     //WordCount(wordCount);
+    //
+    std::vector<struct WordCount> vecWordCount;
+    //struct WordCount tmp;
+    //for (auto it : wordCount) {
+    //    tmp.word = it.first;
+    //    tmp.count = it.second;
+    //    vecWordCount.push_back(tmp);
+    //}
 
+    std::fstream outFile;
+    outFile.open("result2.txt", std::ios::out | std::ios::trunc);
+
+    struct WordCount tmp;
+    tmp.count = 3;
+    tmp.word = "C";
+    vecWordCount.push_back(tmp);
+    tmp.count = 1;
+    tmp.word = "A";
+    vecWordCount.push_back(tmp);
+    tmp.count = 4;
+    tmp.word = "D";
+    vecWordCount.push_back(tmp);
+    tmp.count = 2;
+    tmp.word = "B";
+    vecWordCount.push_back(tmp);
+
+    int count = vecWordCount.size();
+    auto i = vecWordCount.begin(), j = vecWordCount.end();
+    int n = 0;
+    std::string tmpWord = vecWordCount[0].word;
+    int tmpCnt = vecWordCount[0].count;
+    int t = 0;
+    std::string tmpStr;
+    
+    //while (n < count) {
+    //    i = vecWordCount.begin();
+    //    j = vecWordCount.end() - 1;
+    //    while (i != j) {
+    //        if (j->count <= tmpCnt) {
+    //            j--;
+    //        }
+    //        else {
+    //            i++;
+    //        }
+
+    //        if (i == j) {
+    //            break;
+    //        }
+
+    //        if (i->count >= tmpCnt) {
+    //            i++;
+    //        }
+    //        else {
+    //            j--;
+    //        }
+
+    //        if (i == j) {
+    //            break;
+    //        }
+    //    }
+
+    //    t = vecWordCount[i - vecWordCount.begin()].count;
+    //    vecWordCount[i - vecWordCount.begin()].count = tmpCnt;
+    //    tmpCnt = t;
+
+    //    tmpStr = vecWordCount[i - vecWordCount.begin()].word;
+    //    vecWordCount[i - vecWordCount.begin()].word = tmpWord;
+    //    tmpWord = tmpStr;
+    //    n++;
+    //}
+
+    for (auto it : vecWordCount) {
+        outFile << it.count << "  " << it.word.c_str() << "\n";
+    }
+
+    outFile.close();
     //3. 排序
     // 3, 1, 5, 2, 4, 6
     // i              j
     // 快速排序，从大到小
-    int a[] = { 3, 1, 5, 2, 4, 6 };
-    int tmp = a[0];
-    int t;
-
-    int i = 0, j = 5;
-    int n = 0;
-    while (n < 5) {
-        i = 0;
-        j = 5;
-        while (i != j) {
-            if (tmp > a[j])
-                j--;
-            else
-                i++;
-
-            if (i == j) {
-                t = a[i];
-                a[i] = tmp;
-                tmp = t;
-                break;
-            }
-
-            if (tmp < a[i])
-                i++;
-            else
-                j--;
-
-            if (i == j) {
-                t = a[i];
-                a[i] = tmp;
-                tmp = t;
-                break;
-            }
-        } 
-        n++;
+    int a[] = { 3, 1, 4, 2, 2, 6, 8, 11, 3};
+    int cnt = sizeof(a) / sizeof(int);
+    QuickSort(a, 0, cnt);
+    for (int i = 0; i < cnt; ++i) {
+        std::cout << a[i] << std::endl;
     }
 }
